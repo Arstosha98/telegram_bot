2d54add272d2cae3c737a2f498737267b831ba60
Ars_tosha <arsentev98pochta@mail.ru>
2023-10-06 16:29:53 +0500

    Парсить сообщения с создаваемым напоминанием и сохранять их в БД

--- src/main/resources/liquibase/scripts/notification.sql
+++ src/main/resources/liquibase/scripts/notification.sql
@@ -1,8 +1,8 @@
 -- liquibase formatted sql
 
--- changeset ars: create table
+-- changeset ars:1
 create table notification_task(
-    id big serial primary key,
+    id bigserial primary key,
     chat_id int8 not null,
     message text not null,
     exec_date timestamp not null
